e932d866313ba23ed8b6e9a1eda1fa1e31b6e42f
Carlotaml21 <carlotaml21@gmail.com>
2025-03-09 10:24:42 +0100

    add transcribe method (TDD:GREEN)

--- src/main/java/com/carlotaml21/DnaToRnaConverter.java
+++ src/main/java/com/carlotaml21/DnaToRnaConverter.java
@@ -1,4 +1,19 @@
 package com.carlotaml21;
 
 public class DnaToRnaConverter {
+
+    public String transcribe(String dna){
+        StringBuilder rna = new StringBuilder();
+        for (char nucleotide : dna.toCharArray()) {
+            switch (nucleotide) {
+                case 'G': rna.append('C'); break;
+                case 'C': rna.append('G'); break;
+                case 'T': rna.append('A'); break;
+                case 'A': rna.append('U'); break;
+                default:
+                    throw new IllegalArgumentException("Invalid DNA nucleotide: " + nucleotide);
+            }
+        }
+        return rna.toString();
+    }
 }
